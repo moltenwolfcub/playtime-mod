42274efac69d8be6373157146d2df7f8deebf9ce
MoltenWolfCub <oliver@tomlins.org.uk>
2022-01-30 14:48:21 +0000

    made a functioning mod file

---
+++ src/main/java/com/moltenwolfcub/playtime/Playtime.java
@@ -0,0 +1,24 @@
+package com.moltenwolfcub.playtime;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+@Mod("playtime")
+public class Playtime
+{
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public Playtime() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event){}
+}
